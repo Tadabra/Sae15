4d9a0e79257f32e465feef4d949e5961c3440c2e
“nsi.pjb” <nsi.pjb@gmail.com>
2022-01-16 23:23:43 +0100

    tp1

--- Tp1/TP1td.py
+++ Tp1/TP1td.py
@@ -19,8 +19,6 @@ def lecture_fichier(chemin: str):
 
 def select(textics: str, chaineSelect) :
     """
-    
-
     Parameters
     ----------
     textics : C'est le string fournis par lecture_fichier.
@@ -52,38 +50,56 @@ def formatageHeure(heure: str):
     return ":".join(table)
 
 
-
-
-
 #textics = lecture_fichier("evenementSAE_15.ics")
 def tpcsv(chaine_evenement: str):
-    tabpropriete = ["UID:","DTSTART:","DTEND:","DESCRIPTION:","SUMMARY:","LOCATION:"] 
+    tabpropriete = ["UID:","DTSTART:","DTEND:","DESCRIPTION:","SUMMARY:","LOCATION:"]#tableau des données à récupérer 
     tab = []    
-    for propriete in tabpropriete :
+    for propriete in tabpropriete :# récupération des données
         tab.append(select(chaine_evenement, propriete))
+    #les données facile à récupérer
+    uid=tab[0]#uid
+    summary=tab[4]#nom evenement
+    salle=tab[5]#lien
+    #les données facile trouver mais à formater
+    dateDebut = separerDateHeure(tab[1])[0]#récupération de la date et formatage
+    heureDébut = separerDateHeure(tab[1])[1]#récupération de l'heure et formatage
+    heureFin = separerDateHeure(tab[2])[1]#récupération de l'heure de fin et formatage pour calculer la durée
+    duree = str(int(heureFin)-int(heureDébut))#calcul de la durée
     
+    #données à extraire de la description tab[3]
     description=tab[3]#on a "RT-TP B1, BOULEUX GUILLAUME"
+    tabDescription = description.split(',')
+    print
+    tabGroupe = []
+    tabProf = []
+    for cas in tabDescription:
+        print(cas)
+        if cas.startswith('RT1-'):
+            if cas[:6]=='RT1-TP':
+                typeActivité = 'TP'
+                tabGroupe.append(cas[6:])
+            elif cas[:6]=='RT1-TD':
+                typeActivité = 'TD'
+                tabGroupe.append(cas[6:])
+            elif cas[:6]=='RT1-CM':
+                typeActivité = 'CM'
+                tabGroupe.append(cas[6:])
+            else:
+                typeActivité = 'vide'
+                tabProf.append(cas)
     # descriptionTab=re.split("-|,",description)# ici on coupe à - et , pour avoir "RT" "TP B1", "BOULEUX GUILLAUME"
     # if len(descriptionTab) == 3:
     #     tabActiviteGroupe = ""
     # else:
     #     tabActiviteGroupe=descriptionTab[1].split(" ")#ça correspond à  "TP" "B1"
     #récupération des propriétés à afficher dans pseudo csv
-    uid=tab[0]
-    dateDebut = separerDateHeure(tab[1])[0]
-    heureDébut = separerDateHeure(tab[1])[1]
-    heureFin = separerDateHeure(tab[2])[1]
-    duree = str(int(heureFin)-int(heureDébut))
-    # typeActivité = tabActiviteGroupe[0]
-    typeActivité = str(description)
-    summary=tab[4]
-    salle=tab[5]
+
     # if len(descriptionTab) > 2:
     #      prof=descriptionTab[2]
     # else:
     #      prof=""
-    prof= str(description)
-    groupe = str(description)
+    prof= str(tabProf)
+    groupe = str(tabGroupe)
     # if len(tabActiviteGroupe) == "":
     #     groupe = ""
     # else:
